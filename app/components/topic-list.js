f70354a0adbac0447351bd2f9870d6b1ada753f6
Thomas Sauer <t-sauer@outlook.de>
2018-03-24 23:16:53 +0100

    Added sorting to list

--- app/components/topic-list.js
+++ app/components/topic-list.js
@@ -1,4 +1,19 @@
 import Component from '@ember/component';
+import { computed } from '@ember/object';
 
 export default Component.extend({
+
+  sortKey: 'label',
+
+  sortedTopics: computed('topics.@each.{label,volume,sentimentScore}', 'sortKey', function() {
+    const topics = this.topics.slice();
+
+    return topics.sortBy(this.sortKey);
+  }),
+
+  actions: {
+    sort(key) {
+      this.set('sortKey', key);
+    }
+  }
 });
